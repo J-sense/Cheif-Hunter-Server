21153565b76224c17bec16d3e1f722f4178d9029
J-sense <jishan1873@gmail.com>
2023-05-04 03:16:12 -0700

    daynmic routing

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ const express = require('express')
 const app = express()
 const cors =require('cors')
 const alldata =require('./data/datas.json')
-const port = 3000
+const port = process.env.PORT || 3000 ;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
+
 app.use(cors());
 app.get('/alldata', (req,res)=>{
     res.send(alldata)
+});
+app.get('/alldata/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const selectedId = alldata.find(c => parseInt(c.id) === id);
+  res.send(selectedId)
 })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
